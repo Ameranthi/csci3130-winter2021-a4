b69e2e1e8849f23473a256f7e1df675ea277be64
jolmstead <jl260629@dal.ca>
2021-03-20 13:22:07 -0300

    fixed buggy and missing code in factory producer, itermFactory and goodsFactory classes

--- app/src/main/java/ca/dal/cs/csci3130/a4/q3/GoodsFactory.java
+++ app/src/main/java/ca/dal/cs/csci3130/a4/q3/GoodsFactory.java
@@ -3,7 +3,14 @@ package ca.dal.cs.csci3130.a4.q3;
 public class GoodsFactory implements ItemFactory {
     @Override
     public Item getItem(String itemType) {
-        //missing code + buggy code
+        if (itemType.equalsIgnoreCase(ItemConstants.BABY_TOY)){
+            return new BabyToy();
+        } else if (itemType.equalsIgnoreCase(ItemConstants.CLOTHES)) {
+            return new Clothes();
+        } else if(itemType.equalsIgnoreCase(ItemConstants.COMPUTER_PARTS)){
+            return new ComputerParts();
+        }
+
         return null;
     }
 }
--- app/src/main/java/ca/dal/cs/csci3130/a4/q3/SmallTaskFactory.java
+++ app/src/main/java/ca/dal/cs/csci3130/a4/q3/SmallTaskFactory.java
@@ -4,7 +4,13 @@ public class SmallTaskFactory implements ItemFactory {
 
     @Override
     public Item getItem(String itemType) {
-        //missing code + buggy code
+        if (itemType.equalsIgnoreCase(ItemConstants.WALK_A_DOG)){
+            return new WalkADog();
+        } else if (itemType.equalsIgnoreCase(ItemConstants.MOW_THE_LAWN)) {
+            return new MowTheLawn();
+        } else if(itemType.equalsIgnoreCase(ItemConstants.PICK_UP_GROCERY)){
+            return new PickUpGrocery();
+        }
         return null;
     }
 }
